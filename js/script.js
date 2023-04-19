4c0fc3de596ac5b1e0de24c436fd14030a36bd04
Alga Mau <122839503+MaoShizhong@users.noreply.github.com>
2023-04-19 17:42:34 +0100

    Add gameFlow object

--- js/script.js
+++ js/script.js
@@ -8,6 +8,38 @@ const gameBoard = (() => {
     return { board, addToBoard };
 })();
 
+const gameFlow = ((pOne, pTwo) => {
+    let activePlayer = pOne;
+
+    // for DOM to disply current player
+    const getActivePlayer = () => activePlayer;
+    
+    const changeTurns = () => {
+        activePlayer = (activePlayer === pOne) ? pTwo : pOne;
+    };
+
+    // for console before DOM developed
+    const printBoard = () => {
+        for (let i = 0; i < 3; i += 3) {
+            console.log(`${gameBoard.board[i]} ${gameBoard.board[i + 1]} ${gameBoard.board[i + 2]}`);
+        }
+    };
+
+    const playTurn = (pos) => {
+        console.log(`${activePlayer}'s turn!`);
+        gameBoard.addToBoard(activePlayer.token, pos);
+        printBoard();
+        changeTurns();
+    };
+
+    return { getActivePlayer, playTurn };
+})();
+
+// to update DOM display
+// const displayController = (() => {
+
+// })();
+
 
 const playerFactory = (controller, token) => {
     return { controller, token };
